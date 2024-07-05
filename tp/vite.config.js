fc680efdaf15d085d537f746a4105d8bc48efc13
Martin Kloeckner <mjkloeckner@gmail.com>
2024-07-05 13:48:53 -0300

    add vite config file: multiple pages support
    See Vite Docs: [Building for Production - Multi-Page App](https://vitejs.dev/guide/build.html#multi-page-app)

---
+++ tp/vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main:      resolve(__dirname, 'index.html'),
+				bridge:    resolve(__dirname, 'bridge.html'),
+				rails:     resolve(__dirname, 'rails.html'),
+				scene:     resolve(__dirname, 'scene.html'),
+				terrain:   resolve(__dirname, 'terrain.html'),
+				trackMap: resolve(__dirname, 'track-map.html'),
+				train:     resolve(__dirname, 'train.html'),
+				trees:     resolve(__dirname, 'trees.html'),
+				tunnel:    resolve(__dirname, 'tunnel.html')
+			},
+		},
+	},
+})
